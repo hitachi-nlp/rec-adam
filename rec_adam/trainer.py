563ef8cd4a1b3cae416bf4eebcdf8985c5932749
MorishT <magi.magilmagi@gmail.com>
2024-07-04 22:30:31 +0900

    [update]

--- rec_adam/trainer.py
+++ rec_adam/trainer.py
@@ -23,21 +23,26 @@ class RecAdamTrainer(Trainer):
         self.rec_adam_fisher_coef = rec_adam_fisher_coef
 
     def create_optimizer(self):
-        opt_model = self.model_wrapped if is_sagemaker_mp_enabled() else self.model
         if self.optimizer is None:
-            self.optimizer = build_rec_adam_optimizer(
-                opt_model,
-
-                learning_rate=self.args.learning_rate,
-                adam_epsilon=self.args.adam_epsilon,
-                adam_beta1=self.args.adam_beta1,
-                adam_beta2=self.args.adam_beta2,
-                weight_decay=self.args.weight_decay,
-
-                target_task_weight=self.rec_adam_target_task_weight,
-                fisher_coef=self.rec_adam_fisher_coef,
-            )
-            
-        if is_sagemaker_mp_enabled():
-            self.optimizer = smp.DistributedOptimizer(self.optimizer)
+            self.optimizer = build_optimizer_from_trainer(self,
+                                                          self.rec_adam_target_task_weight,
+                                                          self.rec_adam_fisher_coef)
         return self.optimizer
+
+
+def build_optimizer_from_trainer(trainer: Trainer, rec_adam_target_task_weight=1.0, rec_adam_fisher_coef=3000):
+    optimizer = build_rec_adam_optimizer(
+        trainer.model_wrapped if is_sagemaker_mp_enabled() else trainer.model,
+
+        learning_rate=trainer.args.learning_rate,
+        adam_epsilon=trainer.args.adam_epsilon,
+        adam_beta1=trainer.args.adam_beta1,
+        adam_beta2=trainer.args.adam_beta2,
+        weight_decay=trainer.args.weight_decay,
+
+        target_task_weight=rec_adam_target_task_weight,
+        fisher_coef=rec_adam_fisher_coef,
+    )
+    if is_sagemaker_mp_enabled():
+        optimizer = smp.DistributedOptimizer(optimizer)
+    return optimizer
